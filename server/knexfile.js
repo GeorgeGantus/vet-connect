9d7fe220ca3dc0a909b9887decd627110114ec9e
George Gantus <georgegantus@usp.br>
2025-11-12 01:37:13 -0300

    chore: add production postgres

--- server/knexfile.js
+++ server/knexfile.js
@@ -14,6 +14,20 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     }
+  },
+
+  production: {
+    client: 'pg', // PostgreSQL client
+    connection: {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      // For Cloud Run, connect via a Unix socket
+      host: process.env.DB_HOST,
+    },
+    migrations: {
+      directory: './db/migrations'
+    }
   }
 
 };
