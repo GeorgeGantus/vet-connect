93807d1f9e97ac1504a8c0b15924e6454b73e3d9
George Gantus <georgegantus@usp.br>
2025-11-12 02:40:10 -0300

    fix: front end build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: `${import.meta.env.VITE_PUBLIC_URL}/`,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    // Use process.env or env object to access the variables
+    base: env.VITE_PUBLIC_URL || '/',
+  }
 })
